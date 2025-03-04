edcc1527183f36065a812916fd74d965c790b912
lequentin <quentin.letessier@ynov.com>
2025-03-04 09:00:54 +0100

    python

--- person.py
+++ person.py
@@ -1,13 +1,13 @@
 ## person.py
-from abc import ABC
+from abc import ABC # import de la class ABC pour creer une classe abstraite en python j'ai pas tous compris a re regarder pas capter pk ABC specifiquement mais aze sa marche pas sans sa 
 
 # Abstract class Person
-class Person(ABC):
+class Person(ABC):  # class personne qui herite de ABC 
     def __init__(self, name, age):
         self._name = name  # Encapsulation
         self._age = age
 
-    def get_name(self):
+    def get_name(self): # le petit gette pour acceder au attributs 
         return self._name
     
     def get_age(self):
--- student.py
+++ student.py
@@ -1,16 +1,16 @@
 ## student.py
-from person import Person
+from person import Person # import de la class person 
 
-class Student(Person):
-    def __init__(self, name, studentID, age):
-        super().__init__(name, age)
+class Student(Person): # elle herite de person donc elle posede c attribu 
+    def __init__(self, name, studentID, age): # dabord person 
+        super().__init__(name, age) #
         self._studentID = studentID
         self._grades = []
     
-    def add_grade(self, grade):
+    def add_grade(self, grade): # on ajoute une note a la liste grade 
         self._grades.append(grade)
     
-    def get_average_grade(self):
+    def get_average_grade(self): # 
         return sum(self._grades) / len(self._grades) if self._grades else 0
 
 ## undergraduate_student.py
